106c24d0c52197bc3b971526b2a32c7c0c3b6e27
Andrew Mcveigh <me@andrewmcveigh.com>
2014-01-07 10:03:32 +0100

    Update deps, project.clj

--- project.clj
+++ project.clj
@@ -4,11 +4,9 @@
   :url "http://github.com/andrewmcveigh/very-fine-uploader"
   :license {:name "GPLv3"
             :url "http://www.gnu.org/licenses/gpl.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/data.json "0.2.2"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.3"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.3"]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.4"]]
   :plugins [[lein-ring "0.8.2"]]
-  :ring {:handler very-fine-uploader.core/app}
-  :repositories [["snapshots" {:url "https://clojars.org/repo/" :creds :gpg}]
-                 ["releases" {:url "https://clojars.org/repo/" :creds :gpg}]])
+  :ring {:handler very-fine-uploader.core/app})
